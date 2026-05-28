a6c8e21c098b71a3d46159432bc4a9a3b8b02356
andaryx <slepcikjaroslav892@gmail.com>
2026-05-28 15:36:57 +0200

    hotovo xd

--- js/otazka-one.js
+++ js/otazka-one.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
        
         if (jeSpravne) {
-            vysledokElement.textContent = "Správna odpoveď! Peter Sagan sa preslávil  cyklistikov";
+            vysledokElement.textContent = "Správna odpoveď! Marek Hamšík sa preslavil cyklistikov";
             vysledokElement.style.color = "#108702"; // Zelená pre úspech
         } 
 
