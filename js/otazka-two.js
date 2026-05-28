87d981d60b62da181895a2a9dbb851b1388bfb6b
andaryx <slepcikjaroslav892@gmail.com>
2026-05-28 15:45:12 +0200

    čus

--- js/otazka-two.js
+++ js/otazka-two.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
        
         if (jeSpravne) {
-            vysledokElement.textContent = "Správna odpoveď! Marek Hamšík sa preslavil cyklistikov";
+            vysledokElement.textContent = "Správna odpoveď! Ivan Bella  bol prvý slovenský kozmonaut.";
             vysledokElement.style.color = "#108702"; // Zelená pre úspech
         } 
 
